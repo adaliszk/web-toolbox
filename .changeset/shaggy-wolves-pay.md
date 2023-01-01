85b0a77fd197a9e83de2a779f3824319d0d3aa62
adaliszk <adaliszk@gmail.com>
2023-01-01 14:42:10 +0000

    docs(changeset): Update tooling to reflect my latest workflow

---
+++ .changeset/shaggy-wolves-pay.md
@@ -0,0 +1,8 @@
+---
+"@adaliszk/eslint-config-typescript": patch
+"@adaliszk/typescript-testing": patch
+"@adaliszk/typescript": patch
+"@adaliszk/eslint": patch
+---
+
+Update tooling to reflect my latest workflow
--- packages/typescript-testing/source/typescript-testing.ts
+++ packages/typescript-testing/source/typescript-testing.ts
@@ -1,29 +1,24 @@
-import * as testdeckSrc from '@testdeck/jest'
+// noinspection JSUnusedGlobalSymbols
+
+import * as testdeckSrc from '@testdeck/vitest'
+import * as vitestSrc from 'vitest'
 import * as chaiSrc from 'chai'
 
-// region Testdeck
-
+// region Library exports
 export const testdeck = testdeckSrc
-
+export const vitest = vitestSrc
+export const chai = chaiSrc
 // suite, test, slow, timeout, retries, pending, only, skip, params
-export * from '@testdeck/jest'
+export * from '@testdeck/vitest'
+// endregion
 
-// BDD naming
+// BDD-style feature mapping
 export const describe = testdeckSrc.suite
 export const does = testdeckSrc.test
 export const it = testdeckSrc.test
-
-// endregion
-
-// region Chai
-
-export const chai = chaiSrc
-
-// expect, should, use, util, assert
 export const expect = chaiSrc.expect
 export const should = chaiSrc.should
 export const use = chaiSrc.use
 export const util = chaiSrc.util
 export const assert = chaiSrc.assert
-
 // endregion
