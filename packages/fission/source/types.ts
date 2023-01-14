1f95c86efe280fb35f7662c830fff35e9fb2df35
adaliszk <adaliszk@gmail.com>
2023-01-14 13:29:00 +0100

    feat(fission): add every handler types

--- packages/fission/source/types.ts
+++ packages/fission/source/types.ts
@@ -1,16 +1,29 @@
-import { Request } from 'express'
+import type { Request, Response } from 'express'
+import type { Server as WsServer, WebSocket as WsClient } from 'ws'
 
 export interface FunctionContext
 {
     request: Request
+    response: Response
+    // TODO: Add URL template params and query string once https://github.com/fission/environments implemented it
 }
 
-export interface FunctionResponse
+export interface FunctionHeader
 {
-    status: number
-    body: object | string | number
+    // TODO: Annotate the possible header fields with a type
+    [index: string]: string[] | string
 }
 
-export type ResponseCallback = (statusCode: number, body: object | string | number) => void
+export interface FunctionResponse<T>
+{
+    headers: FunctionHeader
+    status: number
+    body: T
+}
 
-export type FunctionHandler = (context: FunctionContext, callback?: ResponseCallback) => Promise<FunctionResponse>
+export type ResponseCallback<T> = (statusCode: number, body: T, headers?: FunctionHeader) => void
+export type FunctionCallbackHandler<T> = (context?: FunctionContext, callback?: ResponseCallback<T>) => void
+export type FunctionResponseHandler<T> = (context?: FunctionContext) => Promise<FunctionResponse<T>>
+export type FunctionSocketHandler = (ws: WsServer, clients: Set<WsClient>) => void
+
+export type FunctionHandler<T> = FunctionResponseHandler<T> | FunctionCallbackHandler<T> | FunctionSocketHandler
