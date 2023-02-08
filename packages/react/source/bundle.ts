41118d3c90c38dd615d6022b2459ab9d9666b1a7
adaliszk <adaliszk@gmail.com>
2023-02-08 02:03:55 +0100

    fix(react): remove the needless tsconfig export

--- packages/react/source/bundle.ts
+++ packages/react/source/bundle.ts
@@ -1,12 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import tsconfig from '../tsconfig.json'
 
 export {
     React,
     ReactDOM,
 }
-
-// Export for tsconfig
-// noinspection JSUnusedGlobalSymbols
-export default tsconfig
