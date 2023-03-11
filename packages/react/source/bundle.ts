54fcfe2f6adf9437e66b882fd179795e3c00cc93
adaliszk <adaliszk@gmail.com>
2023-03-11 22:32:34 +0100

    feat(react): add React Router to the bundled tools

--- packages/react/source/bundle.ts
+++ packages/react/source/bundle.ts
@@ -1,7 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import DOM from 'react-dom'
+import DOMClient from 'react-dom/client'
+import DOMServer from 'react-dom/server'
+import Router from 'react-router'
+import DOMRouter from 'react-router-dom'
 
 export {
     React,
-    ReactDOM,
+    DOMClient,
+    DOMServer,
+    DOM,
+    DOMRouter,
+    Router
 }
