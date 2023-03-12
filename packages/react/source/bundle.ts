41da4f1b30721b0fd31d21deed4229fa2b77f41b
adaliszk <adaliszk@gmail.com>
2023-03-12 14:53:13 +0100

    revert(react): switch to 18.2 stable

--- packages/react/source/bundle.ts
+++ packages/react/source/bundle.ts
@@ -2,8 +2,8 @@ import React from 'react'
 import DOM from 'react-dom'
 import DOMClient from 'react-dom/client'
 import DOMServer from 'react-dom/server'
-import Router from 'react-router'
-import DOMRouter from 'react-router-dom'
+import * as Router from 'react-router'
+import * as DOMRouter from 'react-router-dom'
 
 export {
     React,
